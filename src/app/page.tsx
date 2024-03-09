860639a2726c5f9b18dd18f199934654929f97a0
assilbekov <askhat.assilbekov@gmail.com>
2024-03-08 23:57:15 -0300

    delete nextauth from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
-
 export default async function Home() {
   noStore();
   /* const hello = await api.post.hello.query({ text: "from tRPC" });
@@ -61,24 +57,5 @@ export default async function Home() {
         <CrudShowcase /> */}
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const session = await getServerAuthSession();
-  if (!session?.user) return null;
-
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
