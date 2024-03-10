55485dca9e9815d8528e876e7c161b78ffd664a9
assilbekov <askhat.assilbekov@gmail.com>
2024-03-10 01:38:13 -0300

    Skill Icons created

---
+++ src/app/mastery/_components/SkillIcons.tsx
@@ -0,0 +1,301 @@
+import {
+  AppleIcon,
+  AtomIcon,
+  AudioLinesIcon,
+  AudioWaveformIcon,
+  AwardIcon,
+  BackpackIcon,
+  BabyIcon,
+  BadgeCentIcon,
+  BadgeDollarSignIcon,
+  BadgeEuroIcon,
+  BadgePlusIcon,
+  BaggageClaimIcon,
+  BitcoinIcon,
+  BoltIcon,
+  BookAIcon,
+  BrainIcon,
+  BriefcaseIcon,
+  BugIcon,
+  CameraIcon,
+  DatabaseIcon,
+  FlaskConicalIcon,
+  GitGraphIcon,
+  GlobeIcon,
+  GraduationCapIcon,
+  GuitarIcon,
+  HammerIcon,
+  HardDriveIcon,
+  HazeIcon,
+  HeadphonesIcon,
+  LinkIcon,
+  MedalIcon,
+  MailIcon,
+  MapIcon,
+  MicIcon,
+  MicroscopeIcon,
+  NetworkIcon,
+  PaletteIcon,
+  PenIcon,
+  PencilIcon,
+  RadarIcon,
+  RadiationIcon,
+  RadioTowerIcon,
+  RocketIcon,
+  RibbonIcon,
+  SchoolIcon,
+  ScrollIcon,
+  SendIcon,
+  SeparatorVerticalIcon,
+  SnowflakeIcon,
+  StoreIcon,
+  WatchIcon,
+  WaypointsIcon,
+  WebhookIcon,
+  WorkflowIcon,
+  ZapIcon,
+  ScaleIcon,
+  DumbbellIcon,
+  TrophyIcon,
+  FlowerIcon,
+} from "lucide-react";
+
+
+export const skillIcons = [
+  {
+    name: "dumbbell",
+    Icon: DumbbellIcon,
+  },
+  {
+    name: "workflow",
+    Icon: WorkflowIcon,
+  },
+  {
+    name: "school",
+    Icon: SchoolIcon,
+  },
+  {
+    name: "network",
+    Icon: NetworkIcon,
+  },
+  {
+    name: "palette",
+    Icon: PaletteIcon,
+  },
+  {
+    name: "pen",
+    Icon: PenIcon,
+  },
+  {
+    name: "badge-dollar-sign",
+    Icon: BadgeDollarSignIcon,
+  },
+  {
+    name: "guitar",
+    Icon: GuitarIcon,
+  },
+  {
+    name: "bug",
+    Icon: BugIcon,
+  },
+  {
+    name: "camera",
+    Icon: CameraIcon,
+  },
+  {
+    name: "database",
+    Icon: DatabaseIcon,
+  },
+  {
+    name: "mic",
+    Icon: MicIcon,
+  },
+  {
+    name: "apple",
+    Icon: AppleIcon,
+  },
+  {
+    name: "atom",
+    Icon: AtomIcon,
+  },
+  {
+    name: "audio-lines",
+    Icon: AudioLinesIcon,
+  },
+  {
+    name: "audio-waveform",
+    Icon: AudioWaveformIcon,
+  },
+  {
+    name: "award",
+    Icon: AwardIcon,
+  },
+  {
+    name: "backpack",
+    Icon: BackpackIcon,
+  },
+  {
+    name: "baby",
+    Icon: BabyIcon,
+  },
+  {
+    name: "badge-cent",
+    Icon: BadgeCentIcon,
+  },
+  {
+    name: "badge-euro",
+    Icon: BadgeEuroIcon,
+  },
+  {
+    name: "badge-plus",
+    Icon: BadgePlusIcon,
+  },
+  {
+    name: "baggage-claim",
+    Icon: BaggageClaimIcon,
+  },
+  {
+    name: "bitcoin",
+    Icon: BitcoinIcon,
+  },
+  {
+    name: "bolt",
+    Icon: BoltIcon,
+  },
+  {
+    name: "book-a",
+    Icon: BookAIcon,
+  },
+  {
+    name: "brain",
+    Icon: BrainIcon,
+  },
+  {
+    name: "briefcase",
+    Icon: BriefcaseIcon,
+  },
+  {
+    name: "flask-conical",
+    Icon: FlaskConicalIcon,
+  },
+  {
+    name: "git-graph",
+    Icon: GitGraphIcon,
+  },
+  {
+    name: "globe",
+    Icon: GlobeIcon,
+  },
+  {
+    name: "graduation-cap",
+    Icon: GraduationCapIcon,
+  },
+  {
+    name: "hammer",
+    Icon: HammerIcon,
+  },
+  {
+    name: "hard-drive",
+    Icon: HardDriveIcon,
+  },
+  {
+    name: "haze",
+    Icon: HazeIcon,
+  },
+  {
+    name: "headphones",
+    Icon: HeadphonesIcon,
+  },
+  {
+    name: "link",
+    Icon: LinkIcon,
+  },
+  {
+    name: "medal",
+    Icon: MedalIcon,
+  },
+  {
+    name: "mail",
+    Icon: MailIcon,
+  },
+  {
+    name: "map",
+    Icon: MapIcon,
+  },
+  {
+    name: "microscope",
+    Icon: MicroscopeIcon,
+  },
+  {
+    name: "pencil",
+    Icon: PencilIcon,
+  },
+  {
+    name: "radar",
+    Icon: RadarIcon,
+  },
+  {
+    name: "radiation",
+    Icon: RadiationIcon,
+  },
+  {
+    name: "radio-tower",
+    Icon: RadioTowerIcon,
+  },
+  {
+    name: "rocket",
+    Icon: RocketIcon,
+  },
+  {
+    name: "ribbon",
+    Icon: RibbonIcon,
+  },
+  {
+    name: "scroll",
+    Icon: ScrollIcon,
+  },
+  {
+    name: "send",
+    Icon: SendIcon,
+  },
+  {
+    name: "separator-vertical",
+    Icon: SeparatorVerticalIcon,
+  },
+  {
+    name: "snowflake",
+    Icon: SnowflakeIcon,
+  },
+  {
+    name: "store",
+    Icon: StoreIcon,
+  },
+  {
+    name: "watch",
+    Icon: WatchIcon,
+  },
+  {
+    name: "waypoints",
+    Icon: WaypointsIcon,
+  },
+  {
+    name: "webhook",
+    Icon: WebhookIcon,
+  },
+  {
+    name: "zap",
+    Icon: ZapIcon,
+  },
+  {
+    name: "scale",
+    Icon: ScaleIcon,
+  },
+  {
+    name: "trophy",
+    Icon: TrophyIcon,
+  },
+  {
+    name: "flower",
+    Icon: FlowerIcon,
+  },
+];
