fa75f4bc79a3d6ab1932be772a4ac104e8d511f3
assilbekov <askhat.assilbekov@gmail.com>
2024-03-11 01:14:29 -0300

    timer component styled

--- src/app/mastery/_components/Timer.tsx
+++ src/app/mastery/_components/Timer.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import { useState, useEffect } from 'react';
+import { Label } from '~/components/ui/label';
 
 export const Timer = () => {
   const [timer, setTimer] = useState(0);
-  
-  const minutes = timer % 60;
-  const hours = Math.floor(timer / 60) % 24;
-  const days = Math.floor(timer / 60 / 24);
+
+  const seconds = timer % 60;
+  const minutes = Math.floor(timer / 60) % 60;
+  const hours = Math.floor(timer / 3600) % 24;
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -18,9 +19,12 @@ export const Timer = () => {
   }, []);
 
   return (
-    <div className="timer">
-      <h1>Timer</h1>
-      <p>{days} days {hours} hours {minutes} minutes</p>
+    <div className="w-48 h-48 bg-gradient-to-r from-cyan-500 via-purple-500 to-emerald-500 rounded-full flex justify-center items-center p-2">
+      <div className="bg-white w-full h-full rounded-full flex justify-center items-center">
+        <Label className="text-xl">
+          {hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
+        </Label>
+      </div>
     </div>
-  );
+  )
 };
