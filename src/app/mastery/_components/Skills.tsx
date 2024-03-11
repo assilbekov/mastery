a22c54e7d3b3e4c05b162339a1834cc13eaf129d
assilbekov <askhat.assilbekov@gmail.com>
2024-03-11 14:33:07 -0300

    skillsWithTotalHours added

--- src/app/mastery/_components/Skills.tsx
+++ src/app/mastery/_components/Skills.tsx
@@ -9,6 +9,7 @@ import { TimeBlocksTable } from "./TimeBlocksTable";
 import { RocketIcon } from "lucide-react";
 import { Label } from "~/components/ui/label";
 import { CreateSkillDialog } from "./CreateSkillDialog";
+import { useMemo } from "react";
 
 type SkillsProps = {
   initialSkills: InferSelectModel<typeof skillsSchema>[];
@@ -18,6 +19,20 @@ type SkillsProps = {
 export const Skills = ({ initialSkills, initialTimeBlocks }: SkillsProps) => {
   const { data: skills } = api.skill.getAllByUserId.useQuery(undefined, { initialData: initialSkills });
   const { data: timeBlocks } = api.timeBlock.getAllByUserId.useQuery(undefined, { initialData: initialTimeBlocks });
+
+  const skillsWithTotalHours = useMemo(() => {
+    return skills.map(skill => {
+      const timeBlocksForSkill = timeBlocks.filter(timeBlock => timeBlock.skillId === skill.id);
+      const totalHours = Number(
+        (timeBlocksForSkill.reduce((acc, timeBlock) => acc + timeBlock.timeInSeconds, 0) / 3600)
+          .toFixed(2)
+      );
+      return {
+        skill,
+        totalHours,
+      }
+    });
+  }, [skills, timeBlocks])
 
   return (
     <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
@@ -35,8 +50,8 @@ export const Skills = ({ initialSkills, initialTimeBlocks }: SkillsProps) => {
         </Alert>
       ) : (
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-          {skills.map(skill => (
-            <SkillCard key={skill.id} skill={skill} totalHours={63.2} />
+          {skillsWithTotalHours.map(({ skill, totalHours }) => (
+            <SkillCard key={skill.id} skill={skill} totalHours={totalHours} />
           ))}
         </div>
       )}
