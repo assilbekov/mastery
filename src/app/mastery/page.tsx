e72ef2df0f976ecd9bb0e7a59ed8a0dbca5dd879
assilbekov <askhat.assilbekov@gmail.com>
2024-03-11 14:18:54 -0300

    Page component fixes

--- src/app/mastery/page.tsx
+++ src/app/mastery/page.tsx
@@ -1,17 +1,9 @@
-import { InferSelectModel } from "drizzle-orm";
-import { RocketIcon } from "lucide-react";
 import { unstable_noStore as noStore } from "next/cache";
-import Link from "next/link";
-import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
-import { Button } from "~/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import { CreateSkillDialog } from "./_components/CreateSkillDialog";
 import 'react-time-picker/dist/TimePicker.css';
 import 'react-clock/dist/Clock.css';
 import { api } from "~/trpc/server";
-import { SkillCard } from "./_components/SkillCard";
 import { Skills } from "./_components/Skills";
-import { Timer } from "./_components/Timer";
+import { UserButton } from "@clerk/nextjs";
 
 export default async function Page() {
   noStore();
@@ -23,142 +15,11 @@ export default async function Page() {
 
   return (
     <div>
-      <div className="flex justify-between items-center px-4 py-3">
-        <h3 className="text-2xl font-semibold">Skills</h3>
-        <CreateSkillDialog />
+      <div className="flex justify-between items-center border-b px-4 py-3">
+        <h3 className="text-2xl font-semibold">Mastery</h3>
+        <UserButton />
       </div>
-      <Alert>
-        <RocketIcon className="h-4 w-4" />
-        <AlertTitle>No skills created!</AlertTitle>
-        <AlertDescription>
-          You have nothing to track here. Create your first skill now.
-        </AlertDescription>
-      </Alert>
       <Skills initialSkills={skills} initialTimeBlocks={timeBlocks} />
-      <Timer />
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        {skills.map(skill => (
-          <SkillCard key={skill.id} skill={skill} totalHours={56.356} />
-        ))}
-        {skills.map((skill) => (
-          <Card>
-            <CardHeader>
-              <CardTitle>{skill.name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="flex items-center space-x-2">
-                <div
-                  className={`w-4 h-4 rounded-full ${skill.color}`}
-                ></div>
-                <p className="text-sm text-muted-foreground">
-                  {skill.icon}
-                </p>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Total Revenue
-            </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-            </svg>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-            <p className="text-xs text-muted-foreground">
-              +20.1% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Subscriptions
-            </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-            </svg>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+2350</div>
-            <p className="text-xs text-muted-foreground">
-              +180.1% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Sales</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <rect width="20" height="14" x="2" y="5" rx="2" />
-              <path d="M2 10h20" />
-            </svg>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
-            <p className="text-xs text-muted-foreground">
-              +19% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Active Now
-            </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+573</div>
-            <p className="text-xs text-muted-foreground">
-              +201 since last hour
-            </p>
-          </CardContent>
-        </Card>
-      </div>
     </div>
   );
 }
